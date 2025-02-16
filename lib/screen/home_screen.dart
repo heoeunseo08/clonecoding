1b6acaad17fba783ef814eb2cc65b66e170d5bb6
heo eunseo <heoeunseong140@gmail.com>
2025-02-17 00:48:11 +0900

    plus body

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -12,17 +13,90 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Map<String, String>> dates = [];
+  List<Map<String, String>> datas = [];
 
   @override
   void initState() {
     super.initState();
+    datas = [
+      {
+        "image": "assets/images/ara-1.jpg",
+        "title": "네메시스 축구화",
+        "location": "제주 제주시 아라동",
+        "price": "30000",
+        "likes": "2",
+      },
+      {
+        "image": "assets/images/ara-2.jpg",
+        "title": "LA갈비 5kg팔아요~",
+        "location": "제주 제주시 아라동",
+        "price": "100000",
+        "likes": "5",
+      },
+      {
+        "image": "assets/images/ara-3.jpg",
+        "title": "치약 팝니다.",
+        "location": "제주 제주시 아라동",
+        "price": "5000",
+        "likes": "0",
+      },
+      {
+        "image": "assets/images/ara-4.jpg",
+        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
+        "location": "제주 제주시  아라동",
+        "price": "2500000",
+        "likes": "6",
+      },
+      {
+        "image": "assets/images/ara-5.jpg",
+        "title": "디월트존기임팩",
+        "location": "제주 제주시  아라동",
+        "price": "150000",
+        "likes": "2",
+      },
+      {
+        "image": "assets/images/ara-6.jpg",
+        "title": "갤럭시s10",
+        "location": "제주 제주시  아라동",
+        "price": "180000",
+        "likes": "2",
+      },
+      {
+        "image": "assets/images/ara-7.jpg",
+        "title": "선반",
+        "location": "제주 제주시  아라동",
+        "price": "15000",
+        "likes": "2",
+      },
+      {
+        "image": "assets/images/ara-8.jpg",
+        "title": "냉장 쇼케이스",
+        "location": "제주 제주시  아라동",
+        "price": "80000",
+        "likes": "3",
+      },
+      {
+        "image": "assets/images/ara-9.jpg",
+        "title": "대우 미니냉장고",
+        "location": "제주 제주시  아라동",
+        "price": "30000",
+        "likes": "3",
+      },
+      {
+        "image": "assets/images/ara-10.jpg",
+        "title": "멜킨스 풀업 턱걸이 판매합니다.",
+        "location": "제주 제주시  아라동",
+        "price": "50000",
+        "likes": "7",
+      },
+    ];
   }
 
   PreferredSizeWidget _appbarWidget() {
     return AppBar(
       title: GestureDetector(
         onTap: () {
+          // ignore: avoid_print
           print('Home Screen');
         },
         child: Row(
@@ -55,10 +129,104 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  final oCcy = NumberFormat("###,###", "ko_KR");
+
+  String calcStringToWin(String priceString) {
+    return "${oCcy.format(int.parse(priceString))} 원";
+  }
+
+  Widget _bodyWidget() {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      // ignore: no_leading_underscores_for_local_identifiers
+      itemBuilder: (BuildContext _context, int index) {
+        // ignore: avoid_unnecessary_containers
+        return Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    datas[index]["image"].toString(),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                // ignore: avoid_unnecessary_containers
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          datas[index]["title"].toString(),
+                          style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          datas[index]["location"].toString(),
+                          style: TextStyle(
+                            fontSize: 12,
+                            // ignore: deprecated_member_use
+                            color: Colors.black.withOpacity(0.4),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          calcStringToWin(datas[index]["price"].toString()),
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/heart_off.svg",
+                              width: 13,
+                              height: 13,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(datas[index]["likes"].toString()),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ));
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 1,
+          // ignore: deprecated_member_use
+          color: Colors.black.withOpacity(0.4),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbarWidget(),
+      body: _bodyWidget(),
     );
   }
 }
